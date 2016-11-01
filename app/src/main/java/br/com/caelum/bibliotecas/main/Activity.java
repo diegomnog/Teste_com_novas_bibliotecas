0fb0b651c5470493582342cbe2772f02f0a924f8
Diego <diegomnogueira@outlook.com>
2016-11-01 16:37:56 -0200

    1 commit

---
+++ app/src/main/java/br/com/caelum/bibliotecas/main/Activity.java
@@ -0,0 +1,20 @@
+package br.com.caelum.bibliotecas.main;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import br.com.caelum.bibliotecas.R;
+
+/**
+ * Created by android6275 on 01/11/16.
+ */
+
+public class Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
